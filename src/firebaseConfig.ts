24dfa5d91f4441511e1d5620ee568e6d1a183fc0
Umut Çalışkan <umutcaliskan@Mac.bbrouter>
2025-03-04 04:43:54 +0300

    Firebase Security Secured

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,16 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCykOQkK07hUbrhiFfl6J8ma7cCb1H4ipQ",
-  authDomain: "resumebuilder-picksoso.firebaseapp.com",
-  projectId: "resumebuilder-picksoso",
-  storageBucket: "resumebuilder-picksoso.firebasestorage.app",
-  messagingSenderId: "934278391962",
-  appId: "1:934278391962:web:6068955921992528ac5723",
-  measurementId: "G-X6Z55TCGQK"
-};
+import { firebaseConfig } from './config';
 
 const app = initializeApp(firebaseConfig);
 
