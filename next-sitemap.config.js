7f14a4b0de086ee0cc4e3da21e0495c10810f0f3
Umut Çalışkan <umutcaliskan@Mac.bbrouter>
2025-04-15 03:08:30 +0300

    Sitemap XML

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://cvgen.com.tr',
+    generateRobotsTxt: true, // robots.txt de üret
+    sitemapSize: 5000, // opsiyonel
+  }
+  
