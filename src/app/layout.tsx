4c071cb428ccab1a63489525911b10de4121dee5
Umut Çalışkan <umutcaliskan@Mac.bbrouter>
2025-05-17 00:17:39 +0300

    Tag Manager Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,25 +39,23 @@ export default function RootLayout({
         <meta name="google-site-verification" content="FKxABNn3I3zdKWJxWkVLTlYtuFin_2PxKFqdsNgwgQ0" />
         <meta name="google-adsense-account" content="ca-pub-8769506745005038" />
         <meta name="yandex-verification" content="cd6cec414a1a3e76" />
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MWBJ9ZV8');
-          `}
-        </Script>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MWBJ9ZV8');`
+        }} />
+        {/* End Google Tag Manager */}
       </head>
       <body className={inter.variable}>
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MWBJ9ZV8"
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MWBJ9ZV8"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }} />
+        {/* End Google Tag Manager (noscript) */}
         <AuthProvider>
           <Header />
           <main>{children}</main>
